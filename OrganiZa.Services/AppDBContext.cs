60dc939dc5a15ff051621a604e0b992747c392ca
MissaelChing <Missa_Ching@hotmail.com>
2020-10-31 19:01:53 -0600

    se agrego una implementacion de session para que se maneje una mejor experiencia

---
+++ OrganiZa.Services/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OrganiZa.Models;
+namespace OrganiZa.Services
+{
+    public class AppDBContext : DbContext
+    {
+        // Propiedades y métodos de la clase.
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<AdministradorModels> Administrador { get; set; }
+        public DbSet<TutorModels> Tutor { get; set; }
+        public DbSet<EscuelaModels> Escuela { get; set; }
+
+
+
+
+    }
+
+}
+
+
+   
+
---
+++ OrganiZa.Services/Controladores/Tutor.cs
@@ -0,0 +1,21 @@
+﻿using OrganiZa.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganiZa.Services.Controladores
+{
+    public class Tutor : SQLRepository<TutorModels>, IRepositoryRegistroTutor
+    {
+        public Tutor(AppDBContext context) : base(context) { }
+
+        public int InsertT(TutorModels tutor)
+        {
+            if (tutor == null) throw new ArgumentNullException("Entity");
+            entities.Add(tutor);
+            context.SaveChanges();
+            return tutor.Id;
+        }
+
+    }
+}
